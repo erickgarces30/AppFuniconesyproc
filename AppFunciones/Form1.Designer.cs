6478540a572a24bab77ad4c77388d63ba1e01d2c
erickgarces30 <erick.garces@pucese.edu.ec>
2019-11-06 12:35:43 -0500

    FUNSION SUMA 2

--- AppFunciones/Form1.Designer.cs
+++ AppFunciones/Form1.Designer.cs
@@ -28,21 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.sumar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ingrese otro entero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(167, 37);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(142, 20);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(167, 90);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(142, 20);
+            this.txtnum2.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(167, 145);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(142, 20);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // sumar
+            // 
+            this.sumar.Location = new System.Drawing.Point(340, 33);
+            this.sumar.Name = "sumar";
+            this.sumar.Size = new System.Drawing.Size(84, 20);
+            this.sumar.TabIndex = 6;
+            this.sumar.Text = "SUMAR";
+            this.sumar.UseVisualStyleBackColor = true;
+            this.sumar.Click += new System.EventHandler(this.sumar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 22);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "CERRAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "RESTA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 20);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "mULTIPL";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(342, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 20);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "SUMAR";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(438, 191);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.sumar);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Button sumar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- AppFunciones/Form1.cs
+++ AppFunciones/Form1.cs
@@ -21,5 +21,27 @@ namespace AppFunciones
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //cierra el formulario
+            this.Close();
+        }
+
+        private void sumar_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(this.txtNum1.Text.ToString());
+            int num2 = Int32.Parse(this.txtnum2.Text.ToString());
+            int suma = fnsuma(num1, num2);
+
+            //asigno  el resultado al cuadro de texto
+            this.txtresultado.Text = suma.ToString();
+        }
+
+        int fnsuma(int a, int b)
+        {
+            return (a + b);
+        }
+
     }
 }
